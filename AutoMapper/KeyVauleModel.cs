be4fbb87325eea25295972b63c25e2eb5795186e
DESKTOP-K8B02EC\USER <nicki831028@gmail.com>
2024-07-04 14:10:48 +0800

    no message

---
+++ AutoMapper/KeyVauleModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapper
+{
+    internal class KeyVauleModel<SourceType, DestType>
+    {
+        public Dictionary<string, string> namemapping = new Dictionary<string, string>();
+
+        public KeyVauleModel()
+        {
+
+        }
+        
+
+        public KeyVauleModel<SourceType, DestType> Formember<SourceProperty>(Expression<Func<SourceType, SourceProperty>> sourceProperty, Expression<Func<DestType, object>> destProperty)
+        {
+            var sourcePropInfo = GetPropertyInfo(sourceProperty);
+            var destPropInfo = GetPropertyInfo(destProperty);
+
+            namemapping[sourcePropInfo.Name] = destPropInfo.Name;
+
+            return this;
+        }
+      
+
+        private PropertyInfo GetPropertyInfo<T, TProperty>(Expression<Func<T, TProperty>> propertyLambda)
+        {
+            
+            if (propertyLambda.Body is MemberExpression member)
+            {
+                if (member.Member is PropertyInfo propInfo)
+                {
+                    return propInfo;
+                }
+            }
+            throw new ArgumentException("Lambda expression must be a property");
+        }       
+    }
+}
--- AutoMapper/Program.cs
+++ AutoMapper/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,32 @@ namespace AutoMapper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            
+            Student student = new Student();
+            student.Name = "Nicki";
+            student.Email = "nicki831028@gmail.com";
+            student.Address = "Tainan";
+            student.Id = 1;
+           
+
+
+            //var keyValueModel = new KeyVauleModel<Student, User>(student, user);
+            //keyValueModel.Formember(sou => sou.Id, dest => dest.id)
+            //            .Formember(sou => sou.Name, dest => dest.name)
+            //            .Formember(sou => sou.Email, dest => dest.Address);
+
+
+            
+
+           User usr =   Mapper.Map<Student, User>(student,config =>
+           {
+               config.Formember(sou => sou.Id, dest => dest.id)
+                        .Formember(sou => sou.Name, dest => dest.name)
+                        .Formember(sou => sou.Email, dest => dest.Address);
+               return config;
+           });
+           
+           Console.ReadKey();
         }
     }
 }
